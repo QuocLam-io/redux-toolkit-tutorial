a3eec9f8d7ba650b7289ef30e152856c913fffd6
frostycakes <lamquoc.24@gmail.com>
2022-10-07 15:07:20 -0400

    clearCart reducer

--- starter/src/store.js
+++ starter/src/store.js
@@ -1,6 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./features/cart/cartSlice";
-console.log(cartReducer)
 
 export const store = configureStore({
   reducer: {
